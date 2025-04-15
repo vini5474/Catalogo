b31e9866de0f5d533f5b655e0a9d197a5f58f61a
Vinicius Egidio <vini5474>
2025-04-14 21:30:17 -0300

    14/04/2025 21:30

--- src/Componentes/card.jsx
+++ src/Componentes/card.jsx
@@ -6,6 +6,7 @@ export function Card({movie}) {
             <h3>{movie.title}</h3>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
             <p>{movie.overview}</p>
+            <button>Ver detalhes</button>
         </div>
     )
 }
--- src/Componentes/footer.jsx
+++ src/Componentes/footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
     return (
         <footer className={estilos.container}>
             <h2>Plataforma de filmes</h2>
-            <h2>Todos os direitos reservados</h2>
+            <h2>&copy; Todos os direitos reservados</h2>
         </footer>
     )
 }
