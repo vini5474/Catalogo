dc5bef89155cb0bfcb8ef30e35f3ed3b279cf186
Vinicius Egidio <vini5474>
2025-04-19 18:44:33 -0300

    19/04/2025 18:44

--- src/Paginas/perfil.jsx
+++ src/Paginas/perfil.jsx
@@ -1,13 +1,24 @@
 import estilos from './perfil.module.css'
 import { FaRegUserCircle } from "react-icons/fa";
+import { CiCirclePlus } from "react-icons/ci";
 
 export function Perfil() {
     return(
         <div className={estilos.container}>
             <h2>Perfil</h2>
-            <div className={estilos.cards}>
-                <FaRegUserCircle className={estilos.user} />
-                <h1>Usuário 1</h1>
+            <div className={estilos.sectionCards}>
+                <div className={estilos.card}>
+                    <FaRegUserCircle className={estilos.user} />
+                    <h1>Usuário 1</h1>
+                </div>
+                <div className={estilos.card}>
+                    <FaRegUserCircle className={estilos.user} />
+                    <h1>Usuário 2</h1>
+                </div>
+                <div className={estilos.card}>
+                    <FaRegUserCircle className={estilos.user} />
+                    <h1>Usuário 3</h1>
+                </div>
             </div>
         </div>
     )
